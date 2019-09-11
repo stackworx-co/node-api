60cb2fc437cb23a93fcb674c0af566c392164b47
rashid-zaheer <rashid@stackworx.co>
2019-09-11 22:28:53 +0500

    Tried connection with db using mysql and learning about sequelizer

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const members = require('./Members');
-
-
+const mysql = require('mysql');
 
 const loge = require('./middleware/logger')
 const app = express();
+//create connection
+const db = mysql.createConnection({
+    host:  'localhost',
+    user: 'root',
+    password : 'password'
+}); 
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected');
+});
+
+app.get('/createdb',(req, res)=>{
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database Created..');
+    });
+});
 
 // app.get('/', (req, res)=>{
 //     //res.send("<h1>Hello World<\h1>");
@@ -16,7 +37,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-//app.use(loge);
+app.use(loge);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
